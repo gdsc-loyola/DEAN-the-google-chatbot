fc447c973ba9d50b1a6b86a1fe8702294222837f
franciswguevara <69579186+franciswguevara@users.noreply.github.com>
2021-01-24 15:57:52 +0800

    Update chatbot.py

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -21,7 +21,7 @@ ACCESS_TOKEN = os.environ['PAGE_ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 bot = Bot(ACCESS_TOKEN)
 headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
-df = ''
+df = {}
 articles = ''
 choice = ''
     
