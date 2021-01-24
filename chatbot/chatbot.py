d00a0079c1048fc7fa32dd51136cb568240f6694
franciswguevara <69579186+franciswguevara@users.noreply.github.com>
2021-01-24 15:50:16 +0800

    Update chatbot.py

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -21,6 +21,7 @@ ACCESS_TOKEN = os.environ['PAGE_ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 bot = Bot(ACCESS_TOKEN)
 headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
+df = ''
 articles = ''
 choice = ''
     
@@ -151,6 +152,7 @@ def push(results:list):
 def receive_message():
 
     #remember list of articles and what are article the user is reading
+    global df
     global articles
     global choice
 
@@ -176,39 +178,49 @@ def receive_message():
                         string = message['message'].get('text').lstrip().split(' ',1)
                         
                         #If the person wants to search something
-                        # if string[0].lower() == 'search':
-                        #     send_message(recipient_id,"Thank you for your search! Let me see what I can find. :)")
-                        #     articles = ''
-                        #     articles = push(links(string[1]))
-                        #     if articles:
-                        #         print(articles)
-                        #         for i in range(len(articles)):
+                        if string[0].lower() == 'search' and len(string) >= 2:
+                            send_message(recipient_id,"Thank you for your search! Let me see what I can find. :)")
+                            articles = push(links(string[1]))
+                            articles.insert(0,1)
+                            if articles:
+                                df[recipient_id] = articles
+                                for i in range(len(articles)):
                                     
-                        #             #Send a button allowing them to read more of the article
-                        #             buttons = [
-                        #                             {
-                        #                                 "type":"postback",
-                        #                                 "title":"Read",
-                        #                                 "payload": i
-                        #                             }
-                        #                         ]
-                        #             #Send the title and summary of the article
-                        #             button_message(recipient_id,articles[i]['title'][0:600],buttons)
-                        #     else:
-                        #         send_message(recipient_id,'''I couldn't find anything on that, could you try making your search more specific? It would help if you asked a question! (Ex. "Who is the President of the Philippines?)''')
-                        # else:
-                        #indent this when top is uncommented
-                        send_message(recipient_id,"Hey, I'm Dean! I allow Filipinos to access Google Search at no cost. This app runs purely on Free Facebook Data.\n\nIf you want to get started, just ask me a question! Make sure you write 'search' before your query. I'm excited to learn with you!\n\nI hope that you continue to stay safe! :)")
-                        sleep(3)
-                        send_message(recipient_id, "Thank you for your interest in me! Due to an influx in responses, I'll be taking a short break for now. See you again tomorrow!")
-                            #sleep(3)
-                            #send_message(recipient_id, "I hope that you continue to stay safe! :)")
+                                    #Send a button allowing them to read more of the article
+                                    buttons = [
+                                                    {
+                                                        "type":"postback",
+                                                        "title":"Read",
+                                                        "payload": i + 1
+                                                    }
+                                                ]
+                                    #Send the title and summary of the article
+                                    button_message(recipient_id,articles[i]['title'][0:500],buttons)
+                            else:
+                                send_message(recipient_id,'''I couldn't find anything on that, could you try making your search more specific? It would help if you asked a question! (Ex. "Who is the President of the Philippines?)''')
+                        #If the person mistakenly just said search
+                        elif string[0].lower() == 'search' and len(string) == 1:
+                            send_message(recipient_id, "Hi there! Make sure that you type 'search' before your question. Ex. search Who is the President of the Philippines?")
+                            #TELL THEM THAT 
+                        #All other cases 
+                        else:
+                            #indent this when top is uncommented
+                            send_message(recipient_id,"Can you say that again? I didn't understand what you said. Make sure that you type 'search' before your question. Ex. search Who is the President of the Philippines?")
+                    #MIGHT BE IN THE WRONG PLACE!
+                    #if user sends us a GIF, photo,video, or any other non-text item
+                    if message['message'].get('attachments'):
+                        #FUTURE DEVELOPMENT THINGOS
+                        pass
                 #If user clicked one of the postback buttons
                 elif message.get('postback'):
                     if message['postback'].get('title'):
                         #If user wants to read a specific article
                         if message['postback']['title'] == 'Read':
+                            #retrieve choice from postback
                             choice = int(message['postback']['payload'])
+                            #update df with new choice
+                            df[recipient_id][0] = choice
+                            #dictionary for buttons
                             buttons = [
                                             {
                                                 "type":"postback",
@@ -216,15 +228,16 @@ def receive_message():
                                                 "payload":choice
                                             }
                                         ]
-                            button_message(recipient_id,articles[choice]['article'][0],buttons)
-                            if len(articles[choice]['article']) == 1:
-                                send_message(recipient_id,articles[choice]['article'][0])
-                                articles[choice]['article'] = "End"
+                            #send button message
+                            if len(df[recipient_id][choice]['article']) == 1:
+                                send_message(recipient_id,df[recipient_id][choice]['article'][0])
+                                df[recipient_id][choice]['article'] = "End"
                                 send_message(recipient_id,"End of Article")
-                            elif articles[choice]['article'] == "End":
+                            elif df[recipient_id][choice]['article'] == "End":
                                 send_message(recipient_id,"End of Article")
                             else:
-                                articles[choice]['article'] = articles[choice]['article'][1:]
+                                button_message(recipient_id,df[recipient_id][choice]['article'][0],buttons)
+                                df[recipient_id][choice]['article'] = df[recipient_id][choice]['article'][1:]
                         #If user wants to read more of the article
                         elif message['postback']['title'] == 'Read more':
                             buttons = [
@@ -234,22 +247,18 @@ def receive_message():
                                                 "payload":choice
                                             }
                                         ]
-                            button_message(recipient_id, articles[choice]['article'][0], buttons)
-                            if len(articles[choice]['article']) == 1:
-                                send_message(recipient_id, articles[choice]['article'][0])
-                                articles[choice]['article'] = "End"
+                            if len(df[recipient_id][choice]['article']) == 1:
+                                send_message(recipient_id, df[recipient_id][choice]['article'][0])
+                                df[recipient_id][choice]['article'] = "End"
                                 send_message(recipient_id, "End of Article")
-                            elif articles[choice]['article'] == "End":
+                            elif df[recipient_id][choice]['article'] == "End":
                                 send_message(recipient_id, "End of Article")
                             else:
-                                articles[choice]['article'] = articles[choice]['article'][1:]
+                                button_message(recipient_id, df[recipient_id][choice]['article'][0], buttons)
+                                df[recipient_id][choice]['article'] = df[recipient_id][choice]['article'][1:]
                 else:
-                    send_message(recipient_id, 'Merry Christmas!')
-                
-                    #if user sends us a GIF, photo,video, or any other non-text item
-                    if message['message'].get('attachments'):
-                        #put something here 
-                        pass
+                    #how does this get triggered
+                    pass
     return "Message Processed"
 
 def verify_fb_token(token_sent):
