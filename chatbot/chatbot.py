31b6fee37c5774402b501030307146982e82041e
GDivino <68019100+GDivino@users.noreply.github.com>
2021-01-24 23:51:00 +0800

    store postback in variable

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -172,7 +172,7 @@ def receive_message():
         recipient_id = str(message['sender']['id'])
 
         #If user sent a message
-        if message.request.get('message'):
+        if message.get('message'):
             if message['message'].get('text'):
                 string = message['message'].get('text').lstrip().split(' ',1)
                 
@@ -214,7 +214,7 @@ def receive_message():
                 pass
                 return "Messaged Processed"
         #If user clicked one of the postback buttons
-        elif message.request.get('postback'):
+        elif message.get('postback'):
             print('DF Keys Existing: ',df.keys())
             print(df)
             if message['postback'].get('title'):
@@ -223,7 +223,8 @@ def receive_message():
                 if df.get(recipient_id):
                     #retrieve choice from postback
                     choice = int(message['postback']['payload'])
-                    recipient_id = str(message['sender']['id'])
+                    postback_response = request.get_json()
+                    recipient_id = str(postback_response['entry'][0]['messaging'][0]['sender']['id'])
                     df[recipient_id] = articles
                     df[recipient_id][0] = choice
                     if message['postback']['title'] == 'Read':
